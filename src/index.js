a0c19ef5e26a90842c57eb13257c06999360faa3
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-06-18 10:01:11 +0700

    set up dotenv

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({ success: true });
+});
 
 app.listen(PORT, () => {
   console.log("running...");
