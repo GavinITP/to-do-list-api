0a1f4159196dbbadbca48d0246e712010e603979
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-06-18 10:11:35 +0700

    connect to mongodb

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
+const DB_URI = process.env.DB_URI;
 
 app.use(express.json());
+
+mongoose
+  .connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
 app.get("/", (_req, res) => {
   res.json({ success: true });
